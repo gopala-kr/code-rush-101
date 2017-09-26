91a61a1cf980b42e675bcbf477e5b3cddbcecaa2
Gopala KR <gopal.rms4@gmail.com>
2017-09-26 14:35:25 +0530

    #add crypto

---
+++ something-learned/Cryptography/crypto/CONTRIBUTING.rst
@@ -0,0 +1,35 @@
+==========================
+Contributing to Crypto 101
+==========================
+
+First of all, thanks for contributing!
+
+Please consider filing separate tickets for separate issues. Having
+many different comments stuck on a single issue makes it extremely
+difficult to discuss them separately. This doesn't count for obviously
+wrong things like markup, grammar or spelling errors.
+
+Screenshots demonstrating issues are extremely helpful and highly
+encouraged.
+
+When mentioning page numbers, please use the page numbers that are
+noted on the page itself. This can be different from the page that a
+PDF reader thinks you're on due to cover pages.
+
+Feel free to submit pull requests, although all of the above counts as
+well.
+
+Style guide
+===========
+
+Pronoun gender
+--------------
+
+Use gender-neutral pronouns where appropriate. For example, when
+talking about an attacker, use "they", "them", et cetera instead of
+"he" and "him" or "she" and "her".
+
+When talking about specific people, use the gender pronoun they
+prefer. Also use gendered pronouns when using placeholder names like
+Alice, Bob, Carol. Mallory could be of either gender, but she's a girl
+in Crypto 101.
---
+++ something-learned/Cryptography/crypto/Illustrations/BlockCipher/create_prp_illustrations.py
@@ -0,0 +1,64 @@
+from __future__ import division
+from random import Random, shuffle
+
+random = Random(0xdeadbeef).random
+
+def unirange(n):
+    """
+    Return n equidistant points between 0 (inclusive) and 1 (not inclusive).
+    """
+    return (i / n for i in xrange(n))
+
+#node_names = "ABCDEFGHIJKLMNOPQRSTUVWXYZ234567"
+node_names = "0123456789ABCDEF"
+node_template = """{name} [fillcolor="{hue} {sat} {val}"];"""
+nodes = " ".join(node_template.format(name=name, hue=hue, sat=0.4, val=1)
+                 for name, hue in zip(node_names, unirange(len(node_names))))
+
+shuffled_names = list(node_names)
+shuffle(shuffled_names, random)
+
+shuffled_names_2 = list(node_names)
+shuffle(shuffled_names_2, random)
+
+def vertices(permutation, reverse=False):
+    if reverse:
+        pairs = zip(node_names, permutation)
+    else:
+        pairs = zip(permutation, node_names)
+
+    return " ".join("{0} -> {1};".format(*pair) for pair in pairs)
+
+graph_template = """
+digraph G {{
+    graph [splines=curved; layout=neato; start={start}; epsilon=0.001];
+    node [style=filled, shape=circle];
+    edge [{edge_style}];
+    {nodes}
+    {vertices}
+}}
+"""
+
+with open("AllNodes.dot", "w") as f:
+    f.write(graph_template.format(nodes=nodes,
+                                  vertices=vertices(node_names[1:] + node_names[0]),
+                                  edge_style="style=invis",
+                                  start=1))
+
+with open("Encryption.dot", "w") as f:
+    f.write(graph_template.format(nodes=nodes,
+                                  vertices=vertices(shuffled_names),
+                                  edge_style="",
+                                  start=5))
+
+with open("Decryption.dot", "w") as f:
+    f.write(graph_template.format(nodes=nodes,
+                                  vertices=vertices(shuffled_names, reverse=True),
+                                  edge_style="",
+                                  start=5))
+
+with open("Encryption2.dot", "w") as f:
+    f.write(graph_template.format(nodes=nodes,
+                                  vertices=vertices(shuffled_names_2),
+                                  edge_style="",
+                                  start=95))
